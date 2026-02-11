2141dc5fc1ce9403c93e1ffe271474f890cb3def
Kartavya Raikwar <kartvayaraikwar@gmail.com>
2026-02-11 15:34:27 +0530

    Comment out plotly.graph_objects import
    Comment out the import statement for plotly.graph_objects.

--- mani.py
+++ mani.py
@@ -8,7 +8,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
-import plotly.graph_objects as go
+# import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
 import warnings
