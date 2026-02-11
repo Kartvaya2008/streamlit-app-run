fa1ecf26bc157c5f4ea76e312978b0ba6f002305
Kartavya Raikwar <kartvayaraikwar@gmail.com>
2026-02-11 15:10:16 +0530

    Comment out import of plotly.graph_objects

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 # import plotly.express as px
-import plotly.graph_objects as go
+# import plotly.graph_objects as go
 from datetime import datetime, timedelta
 from statsmodels.tsa.holtwinters import SimpleExpSmoothing
 
