e57c8beafb0a13d0b7985bc977d3c7035e41d105
Kartavya Raikwar <kartvayaraikwar@gmail.com>
2026-02-11 15:09:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
+# import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime, timedelta
 from statsmodels.tsa.holtwinters import SimpleExpSmoothing
